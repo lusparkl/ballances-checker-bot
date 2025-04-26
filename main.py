24e5966d694554bc9db54d9ab6e858b427478ac3
lusparkl <124379919+lusparkl@users.noreply.github.com>
2025-04-26 16:17:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import os
 
 if __name__ == "__main__":
     print("Webhook server is running...")
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
 
